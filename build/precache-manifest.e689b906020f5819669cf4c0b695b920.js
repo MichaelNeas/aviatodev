a5bc893fbb0c1b62800425fb1ded8918055b9ad0
michaelneas <michael.r.neas@gmail.com>
2019-04-30 21:03:52 -0400

    gh pages update

---
+++ build/precache-manifest.e689b906020f5819669cf4c0b695b920.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b64cb2171266f4ccdda7f0a15784731",
+    "url": "/aviatodev/index.html"
+  },
+  {
+    "revision": "e5b2137fcc7b777f85cc",
+    "url": "/aviatodev/static/css/main.4b738920.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/aviatodev/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "e5b2137fcc7b777f85cc",
+    "url": "/aviatodev/static/js/main.71a439c1.chunk.js"
+  },
+  {
+    "revision": "a347e05f7e501d06738f",
+    "url": "/aviatodev/static/js/runtime~main.4f9b7b57.js"
+  },
+  {
+    "revision": "3c1f915b4ca69511f56812a94ef78cf5",
+    "url": "/aviatodev/static/media/logo.3c1f915b.svg"
+  }
+]);
